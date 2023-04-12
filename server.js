14063011e4c67c12cb5a91232bbf91375b1948e3
Vidyuth Jagadeesan <vidyuthj@email.unc.edu>
2023-04-12 12:33:59 -0400

    Fixed errors hopefully

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import {rpsls_function } from './lib/rpsls.js';
-import {rps_function} from './lib/rpsls.js';
+import {rpsls } from './lib/rpsls.js';
+import {rps} from './lib/rpsls.js';
 import express from 'express';
 import minimist from "minimist";
 
@@ -14,34 +14,34 @@ app.get('/app', function(req, res) {
     res.status(200).send("200 OK");
 });
 app.get('/app/rps', function(req, res) {
-    res.status(200).send(rps_function());
+    res.status(200).send(rps());
 });
 
 app.get('/app/rpsls', function(req, res) {
-    res.status(200).send(rpsls_function());
+    res.status(200).send(rpsls());
 });
 
 app.get('/app/rps/play', function(req, res) {
-    res.status(200).send(rps_function(req.query.shot));
+    res.status(200).send(rps(req.query.shot));
 });
 
 app.get('/app/rpsls/play', function(req, res) {
-    res.status(200).send(rpsls_function(req.query.shot));
+    res.status(200).send(rpsls(req.query.shot));
 });
 
 app.get('/app/rps/play', function(req, res) {
-    res.status(200).send(rps_function(req.body.shot));
+    res.status(200).send(rps(req.body.shot));
 });
 
 app.get('/app/rpsls/play', function(req, res) {
-    res.status(200).send(rpsls_function(req.body.shot));
+    res.status(200).send(rpsls(req.body.shot));
 });
 
 app.get('/app/rps/play/:shot', function(req, res) {
-    res.status(200).send(rps_function(req.params.shot));
+    res.status(200).send(rps(req.params.shot));
 });
 app.get('/app/rpsls/play/:shot', function(req, res) {
-    res.status(200).send(rpsls_function(req.params.shot));
+    res.status(200).send(rpsls(req.params.shot));
 });
 
 app.get('*',(req, res) => {
